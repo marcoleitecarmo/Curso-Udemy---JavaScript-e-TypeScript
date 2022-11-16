857ed22c303c626c9ab13a7dfde78a5c9c7e193f
Marco Antônio Leite do Carmo <marcoleitecarmo@gmail.com>
2022-11-16 15:54:31 -0300

    console.log

--- aula2/index.js
+++ aula2/index.js
@@ -2,4 +2,4 @@ console.log('Marco Antonio');
 console.log(135);
 console.log(15.85);
 
-console.log('Meu nome é "Marco Antonio". Estou apredendo JavaScript às', 6, 'da noite.');
+console.log('Meu nome é "Marco Antonio". Estou apredendo JavaScript às', 16, 'da noite.');
