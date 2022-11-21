6f68b4f9d31cf7c13ebe06ef04f879a7eaf73cef
Marco Antônio Leite do Carmo <marcoleitecarmo@gmail.com>
2022-11-21 20:38:50 -0300

    if, else if e else parte 2

---
+++ aula25/index.js
@@ -0,0 +1,11 @@
+const numero = 12;
+
+if (numero >= 0 && numero <= 5){
+    console.log('O numero esta entre 0 e 5')
+} else if (numero >= 6  && numero <= 8) {
+    console.log('O numero esta entre 6 e 8');
+} else if (numero >= 9  && numero <= 11){
+    console.log('Numero esta entre 9 e 11')    
+} else {
+    console.log('O numero esta fora de 0 e 11')
+}
