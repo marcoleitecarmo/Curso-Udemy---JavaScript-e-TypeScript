2297edf3a7d39cf6c433395edb1f331bee16000c
Marco Antônio Leite do Carmo <marcoleitecarmo@gmail.com>
2022-12-12 18:38:55 -0300

    Aula dif de var, let e const

---
+++ aula31/index.js
@@ -0,0 +1,37 @@
+const verdadeira = true;
+
+// Let tem escopo de bloco { ... bloco }
+// Var só tem escopo de função
+
+// let nome = 'Marco'; // criando
+// var nome2 = 'Marco'; // criando
+
+
+// if (verdadeira) {
+//   let nome = 'Leite'; // criando
+//   var nome2 = 'Rodrigo'; // redeclarando
+
+//   if (verdadeira) {
+//     var nome2 = 'Rita'; // redeclarando
+//     let nome = 'Outra coisa';
+//   }
+// }
+
+// console.log(nome, nome2);  
+
+
+// function falaOi () {
+
+//   if (verdadeira) {
+//     let nome = 'Luiz';
+//     var sobrenome = 'Miranda';
+//   }
+
+//   console.log(sobrenome);
+
+// }
+
+// falaOi();
+
+let sobrenome = 'Miranda';
+console.log(sobrenome);
